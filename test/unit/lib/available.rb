592df34799c9f3ef4418d90381d2197cd7ed4f0c
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2012-07-01 00:22:22 +0200

    Added more tests for Available.

--- test/unit/lib/available.rb
+++ test/unit/lib/available.rb
@@ -16,6 +16,26 @@ suite "Available" do
     end
 
     assert available.satisfied?
+    assert available.has.foo?
+    assert available.has?(:foo)
+    assert available.need.foo!
+    assert available.need!(:foo)
+    assert available.errors.empty?
+  end
+
+  test "A single hash require" do
+    RequireStub.add_available_libs 'bar'
+
+    available = Available.new do
+      requires foo: 'bar'
+    end
+
+    assert available.satisfied?
+    assert available.has.foo?
+    assert available.has?(:foo)
+    assert available.need.foo!
+    assert available.need!(:foo)
+    assert available.errors.empty?
   end
 
   test "A single failed symbol require" do
@@ -24,5 +44,79 @@ suite "Available" do
     end
 
     assert !available.satisfied?
+    assert !available.has.foo?
+    assert !available.has?(:foo)
+    assert_raise LoadError do available.need.foo! end
+    assert_raise LoadError do available.need!(:foo) end
+    assert_equal 1, available.errors.size
+  end
+
+  test "A single failed hash require" do
+    available = Available.new do
+      requires foo: 'bar'
+    end
+
+    assert !available.satisfied?
+    assert !available.has.foo?
+    assert !available.has?(:foo)
+    assert_raise LoadError do available.need.foo! end
+    assert_raise LoadError do available.need!(:foo) end
+    assert_equal 1, available.errors.size
+  end
+
+  test "A single optional symbol" do
+    RequireStub.add_available_libs 'foo'
+
+    available = Available.new do
+      optional :foo
+    end
+
+    assert available.satisfied?
+    assert available.has.foo?
+    assert available.has?(:foo)
+    assert available.need.foo!
+    assert available.need!(:foo)
+    assert available.errors.empty?
+  end
+
+  test "A single optional hash" do
+    RequireStub.add_available_libs 'bar'
+
+    available = Available.new do
+      optional foo: 'bar'
+    end
+
+    assert available.satisfied?
+    assert available.has.foo?
+    assert available.has?(:foo)
+    assert available.need.foo!
+    assert available.need!(:foo)
+    assert available.errors.empty?
+  end
+
+  test "A single failed optional symbol" do
+    available = Available.new do
+      optional :foo
+    end
+
+    assert available.satisfied?
+    assert !available.has.foo?
+    assert !available.has?(:foo)
+    assert_raise LoadError do available.need.foo! end
+    assert_raise LoadError do available.need!(:foo) end
+    assert_equal 1, available.errors.size
+  end
+
+  test "A single failed optional hash" do
+    available = Available.new do
+      optional foo: 'bar'
+    end
+
+    assert available.satisfied?
+    assert !available.has.foo?
+    assert !available.has?(:foo)
+    assert_raise LoadError do available.need.foo! end
+    assert_raise LoadError do available.need!(:foo) end
+    assert_equal 1, available.errors.size
   end
 end
